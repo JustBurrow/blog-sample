214a40bb003de7f239c12a5bf45bbe90343b6562
Just Burrow <just.burrow@lul.kr>
2024-08-25 13:05:56 +0900

    🔊 `Flow` 메서드에 로그 추가.

--- suspendfun/src/main/kotlin/kr/lul/blog/suspendfun/multiemit/FlowFunctionSample.kt
+++ suspendfun/src/main/kotlin/kr/lul/blog/suspendfun/multiemit/FlowFunctionSample.kt
@@ -11,9 +11,12 @@ class FlowFunctionSample {
      * [repeat] 횟수만큼 2초 간격으로 [Instant]를 발행하는 [Flow]를 반환하는 메서드.
      */
     fun call(): Flow<Instant> = flow {
-        repeat(Random.nextInt(1, 10)) {
+        val repeat = Random.nextInt(1, 10)
+        println("#call : repeat=$repeat")
+        repeat(repeat) {
             delay(2_000)
-            emit(Instant.now())
+            val now = Instant.now()
+            emit(now)
         }
     }
 }
--- suspendfun/src/main/kotlin/kr/lul/blog/suspendfun/multiemit/main.kt
+++ suspendfun/src/main/kotlin/kr/lul/blog/suspendfun/multiemit/main.kt
@@ -1,6 +1,7 @@
 package kr.lul.blog.suspendfun.multiemit
 
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.runBlocking
 import java.time.Instant
@@ -11,9 +12,18 @@ fun main(vararg args: String) {
 
     runBlocking {
         println("start   : ${Instant.now()}")
+        println("")
+
         println("suspend : ${suspendFunctionSample.call()}")
+
+        println("\n\n================================================================================\n\n")
+
         flowTarget.call().onEach {
             println("flow    : $it")
         }.collect()
+
+        println("\n\n================================================================================\n\n")
+
+        println("flow2   : ${flowTarget.call().first()}")
     }
 }
