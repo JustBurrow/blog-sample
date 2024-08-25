07877d279fd60c06d5de828d03a0e0fce3116434
Just Burrow <just.burrow@lul.kr>
2024-08-25 13:14:05 +0900

    💡 주석 수정.

--- suspendfun/src/main/kotlin/kr/lul/blog/suspendfun/multiemit/FlowFunctionSample.kt
+++ suspendfun/src/main/kotlin/kr/lul/blog/suspendfun/multiemit/FlowFunctionSample.kt
@@ -8,7 +8,7 @@ import kotlin.random.Random
 
 class FlowFunctionSample {
     /**
-     * [repeat] 횟수만큼 2초 간격으로 [Instant]를 발행하는 [Flow]를 반환하는 메서드.
+     * 임의의 횟수만큼 2초 간격으로 [Instant]를 발행하는 [Flow]를 반환하는 메서드.
      */
     fun call(): Flow<Instant> = flow {
         val repeat = Random.nextInt(1, 10)
